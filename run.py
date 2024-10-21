b836b309177b397ba86fe100fc95d5ea5725993f
Szymon Stypa <szymon@backtick.se>
2024-10-22 00:27:04 +0200

    jesus took the wheel

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from pypika import Query, Table, CustomFunction
-import subprocess
 import zipfile
+import subprocess
 import sqlite3
 import shutil
 import click
@@ -8,17 +7,14 @@ import json
 import re
 import os
 
-from utils import PathBuilder
+from utils import Path, addPredicates
 
 with open ('./config/config.json', 'r') as f:
     config = json.loads(f.read())
 with open ('./config/hierarchy.json', 'r') as f:
     hierarchy = json.loads(f.read())
 
-sort_path = PathBuilder(config.get('sorted_dir'))
-
-Samples = Table('samples')
-Regexp = CustomFunction('REGEXP', ['expr', 'item'])
+output_path = Path(config.get('sorted_dir'))
 
 def init_db():
     print('Initializing DB')
@@ -37,106 +33,67 @@ def init_db():
 
     return db
 
-def reset_filetree():
-    print('Resetting filetree')
-
-    path = sort_path.settle()
-
-    if os.path.isdir(path): shutil.rmtree(path)
-    os.mkdir(path)
-
-def get_samples(db: sqlite3.Connection, cat: dict, sample_type = None):
-    execute = lambda query: db.execute(str(query)).fetchall()
-
-    samples = Table('samples')
-    Regexp = CustomFunction('REGEXP', ['expr', 'item'])
-
-    if 'query' in cat:
-        return execute(cat.get('query'))
-    else:
-        query = Query \
-            .from_(samples) \
-            .select('*') \
-            .where(samples.local_path.notnull()) \
-            .where(Regexp(cat.get('tag_regex'), samples.tags)) \
-            .where(Regexp(cat.get('file_regex'), samples.filename)) \
-
-        if sample_type:
-            query = query.where(samples.sample_type == sample_type)
-
-        return execute(query)
-
-def generate_links(samples: list, *dirs):
-    print(f'Generating symlinks for {os.path.join(*dirs)}')
-
-    for row in samples:
-        sample_path = row[1]
-        filename = row[10]
-        link_path = sort_path.add(*dirs, filename).settle()
-
-        if not os.path.exists(link_path):
-            os.symlink(sample_path, link_path)
 
 def unpack_logs(keep: bool):
     print('Extracting Splice logs')
 
-    log_dir     = config.get('log_dir')
-    zip_fuzzy   = config.get('zip_fuzzy')
-    zip_dir     = config.get('zip_dir')
+    log_path = Path(config.get('log_dir'))
+    zip_path = Path(config.get('zip_dir'))
 
-    if os.path.isdir(log_dir): shutil.rmtree(log_dir)
-    os.mkdir(log_dir)
+    zip_fuzzy = config.get('zip_fuzzy')
+    target = zip_path.fuzzy_find(zip_fuzzy)
 
-    try:
-        target = next(entry.path for entry in os.scandir(zip_dir) if zip_fuzzy in entry.name)
-        if not target: raise Exception()
-    except:
-        print(f'Splice logs not found in {zip_dir}')
+    if not target:
+        print(f'Splice logs not found in {zip_path}')
         exit()
 
-    shutil.copy(target, log_dir)
+    log_path.clear_directory()
+    shutil.copy(target, str(log_path))
 
     with zipfile.ZipFile(target, 'r') as ref:
-        ref.extractall(log_dir)
+        ref.extractall(str(log_path))
 
-    if not keep: os.remove(target)
+    if not keep: os.remove(str(target))
 
-def traverse_hierarchy(db, node, path = None, query = None):
+def traverse_hierarchy(db, node, path = output_path, query = None, chain = None):
     dirs = node.get('dirs')
-    tag_regex = node.get('tag_regex')
-    file_regex = node.get('file_regex')
-    sample_type = node.get('sample_type')
-    custom_where = node.get('where')
     name = node.get('name')
+    catchall = node.get('catchall')
 
-    if not query:
-        query = Query().from_(Samples).select('*').where(Samples.local_path.notnull())
+    query = addPredicates(node, query)
+    samples = []
 
-    if custom_where:
-        query = query.where(CustomFunction(custom_where)())
-    if tag_regex:
-        query = query.where(Regexp(tag_regex, Samples.tags))
-    if file_regex:
-        query = query.where(Regexp(file_regex, Samples.filename))
-    if sample_type:
-        query = query.where(Samples.sample_type == sample_type)
+    print(f"Traversing {name}")
 
-    if not path:
-        path = sort_path.add(name)
-
-    os.mkdir(path.settle())
+    if name:
+        path = path.append(name)
+        os.mkdir(str(path))
 
     if dirs:
-        for subnode in dirs: traverse_hierarchy(db, subnode, path, query)
+        for subnode in dirs:
+            samples = samples + traverse_hierarchy(db, subnode, path, query, chain)
     else:
-        samples = db.execute(query).fetchall()
-        for row in samples:
-            sample_path = row[1]
-            filename = row[10]
-            link_path = path.add(filename).settle()
+        samples = db.execute(str(query)).fetchall()
+        generate_symlinks(samples, path)
 
-            if not os.path.exists(link_path):
-                os.symlink(sample_path, link_path)
+    if catchall:
+        catchall_samples = traverse_hierarchy(db, catchall)
+        duplicates = filter(lambda sample: sample in catchall_samples, samples)
+        for sample in duplicates: catchall_samples.remove(sample)
+        generate_symlinks(catchall_samples, path)
+
+    return samples
+
+def generate_symlinks(samples: list, path: Path):
+    print(f'Generating symlinks for {path}')
+
+    for row in samples:
+        sample_path = row[1]
+        filename = row[10]
+        link_path = str(path.append(filename))
+
+        if not os.path.exists(link_path):
+            os.symlink(sample_path, link_path)
 
 @click.command()
 @click.option('--keep/--no-keep', default=False)
@@ -144,37 +101,9 @@ def traverse_hierarchy(db, node, path = None, query = None):
 def main(keep: bool, reset: bool):
     unpack_logs(keep)
     db = init_db()
-
-    if reset: reset_filetree()
-
-    sorted_samps = {}
-
-    # process samples
-    for name, cat in hierarchy.get('dirs').items():
-        if cat.get('split') and len(cat.get('split')):
-            for sample_type in cat.get('split'):
-                sorted_samps[name] = {}
-                sorted_samps[name][sample_type] = get_samples(db, cat, sample_type)
-                generate_links(sorted_samps[name][sample_type], name, sample_type)
-        else:
-            sorted_samps[name] = get_samples(db, cat)
-            generate_links(sorted_samps[name], name)
-        
-    # catchall
-    catchall_samps = get_samples(db, hierarchy.get('catchall'))
-
-    def remove_existing(samples):
-        duplicates = filter(lambda sample: sample in catchall_samps, samples)
-        for sample in duplicates: catchall_samps.remove(sample)
-
-    for name, value in sorted_samps.items():
-        if type(value) == list:
-            remove_existing(value)
-        else:
-            remove_existing(value.values())
-                
-    generate_links(catchall_samps, hierarchy['catchall']['name'])
-    subprocess.call(f'open {sorted_dir}', shell=True)
+    if reset: output_path.clear_directory()
+    traverse_hierarchy(db, hierarchy)
+    subprocess.call(f'open {output_path}', shell=True)
 
     print('Done')
     
--- utils.py
+++ utils.py
@@ -1,12 +1,54 @@
 import os
+import shutil
 
-class PathBuilder:
+from pypika import CustomFunction, Query, Table
+
+Samples = Table('samples')
+Regexp = CustomFunction('REGEXP', ['expr', 'item'])
+
+class Path:
     def __init__(self, *args):
         self.args = args
 
-    def add(self, *args):
-        return PathBuilder(*self.args, *args)
+    def append(self, *args):
+        return Path(*self.args, *args)
 
-    def settle(self):
+    def fuzzy_find(self, query):
+        path = str(self)
+        file = None
+
+        try:
+            if os.path.isdir(path):
+                file = next(entry.path for entry in os.scandir(path) if query in entry.name)
+        except: pass
+
+        return file
+
+    def clear_directory(self):
+        path = str(self)
+        if os.path.isdir(path): shutil.rmtree(path)
+        os.mkdir(path)
+
+    def __str__(self):
         return os.path.join(*self.args)
+
+def addPredicates(node, query = None):
+    tag_regex = node.get('tag_regex')
+    file_regex = node.get('file_regex')
+    sample_type = node.get('sample_type')
+    custom_where = node.get('where')
+    
+    if not query:
+        query = Query().from_(Samples).select('*').where(Samples.local_path.notnull())
+
+    if custom_where:
+        query = query.where(CustomFunction(custom_where)())
+    if tag_regex:
+        query = query.where(Regexp(tag_regex, Samples.tags))
+    if file_regex:
+        query = query.where(Regexp(file_regex, Samples.filename))
+    if sample_type:
+        query = query.where(Samples.sample_type == sample_type)
+
+    return query
 
